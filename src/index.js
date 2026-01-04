ebabbf604f59a1af21e9cbf0a11b4612d35321ed
Ismail <int.ismailibrahim@gmail.com>
2026-01-04 22:06:41 +0530

    Initial project setup with React structure

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
+      <App />
+    </DndProvider>
+  </BrowserRouter>
+);
