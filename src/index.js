557919595481653055fbfe868c4ac06779d6dd0f
Ismail <int.ismailibrahim@gmail.com>
2026-01-04 22:41:15 +0530

    Fix image paths for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"; // Back to Browser Router
 
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  // The 'basename' tells React we are hosted in this specific folder
+  <BrowserRouter basename="/RightHome.lk">
     <DndProvider backend={HTML5Backend}>
       <App />
     </DndProvider>
